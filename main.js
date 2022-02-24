590914899c859bd42346829a8791926deb1b9035
MartinK <82144965+RaikunGZ@users.noreply.github.com>
2022-02-24 22:44:15 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// API Key: btyrlvwdnygp86fa3ohjtxmar9pje9
+// Client ID: btyrlvwdnygp86fa3ohjtxmar9pje9
+
+// JSON parshing JS
+/*
+    API verwenden
+    LoginUser DB nach einloggen it twitchtoken
+    rules
+*/
+
+
+function bossData (name, level, health, damage, multiplier){
+    this.name = name;
+    this.level = level;
+    this.health = health;
+    this.damage = damage;
+    this multiplier = multiplier;
+}
+
+const testragon = new bossData("Tesragon", 1, 100, 15, 1.15);
+
+
+
+const bossCalc = () => {
+    testragon.health = 
+}
