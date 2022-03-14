8c57d1e63768c64c37b9001f4892a427864a2b9c
Martin <raikunsgamezone@gmx.de>
2022-03-14 10:40:10 +0100

    twitch.js is in

---
+++ server/twitch.js
@@ -0,0 +1,54 @@
+// API Key: btyrlvwdnygp86fa3ohjtxmar9pje9
+// Client ID: btyrlvwdnygp86fa3ohjtxmar9pje9
+// Twitch-API-Client-Secret: zbd1xfm9dmo1u412r5ybajv2nb6ame
+// Key: cbj43s+X1FHjmj1VZomlWKULmbj++t2x5JS2CeCEBU0=  Token -> User Zuordnung
+// RootTwitchHH1!
+// TwitchHH1!
+
+// WERTE
+// Emote = 0.5 * Lv
+// Follow = 2 * Lv
+// Sub = 5 * Lv
+
+// Rechnung: BaseHP * (Multiplier * Lv)
+//                      |
+// Beispiel: Lv 1 = 100 Health = 20 subs || 50 Follows || 200 Emotes
+// Beispiel: je Lv = 100 * Multiplier
+
+// JSON parshing JS
+/*
+    API verwenden
+    LoginUser DB nach einloggen mit twitchtoken
+    rules
+*/
+
+// AUS DB
+// getBossData()
+//   .then((response) => {
+//     console.log("Boss load Done!");
+//   })
+//   .catch((error) => {
+//     console.log("Boss load failed!");
+//     console.error(error);
+//   });
+
+// async function getBossData() {
+ // const response = await fetch("imgName");
+ // const blob = await response.blob();
+ // document.getElementById("bossList").src = imgName;
+// }
+
+// getPräfix()
+//   .then((response) => {
+//     console.log("Boss load Done!");
+//   })
+//   .catch((error) => {
+//     console.log("Boss load failed!");
+//     console.error(error);
+//   });
+
+// async function getPräfix() {
+//   const response = await fetch("btyrlvwdnygp86fa3ohjtxmar9pje9");//erxperimental not important NOW
+//   const blob = await response.prefix();
+//   document.getElementById("bossList").src;
+// }
