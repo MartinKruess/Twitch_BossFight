6fbd0dd249867bc17344fa8fa6b56f88a6904bd2
Martin <raikunsgamezone@gmx.de>
2022-03-15 09:21:12 +0100

    registration start to feed the DB

--- server/aprovements.js
+++ server/aprovements.js
@@ -1,21 +1,4 @@
-// insert LoginData to LoginDB -> success Login route
 
-app.post('/login', (request, response) => {
-    console.log('success')
-    console.log(request.body)
-    console.log(response)
-    const loginNameAprove = request.body.loginName;
-    const loginPWAprove = request.body.loginPW
-    const loginObjInDB = loginDB.find(request.body.loginName)
-    const isLogedIn = (loginNameAprove === loginObjInDB.loginName
-    && loginPWAprove === loginObjInDB.loginPW) ? true : false;
-  
-    return response.json({
-        isLogedIn,
-        _id: loginObjInDB._id,
-        // nutzename: loginObjInDB.nutzename,
-    });
-  })
 
 // insert registerData to LoginDB -> successfull registrated register route
 app.post('/register', (request, response) => {
--- server/server.js
+++ server/server.js
@@ -4,12 +4,6 @@ const Datastore = require('nedb');
 const cors = require('cors'); // Sicherheit Ben! nachlesen
 const isLogedIn = false;
 
-loginDB.insert.loginName = "Martin";
-loginDB.insert.loginPW = "2222";
-
-// Routes
-const aprovements = require("./aprovements");
-app.use("/aprovements", aprovements);
 
 // Express to function -> listen@all
 const app = express();
@@ -18,11 +12,18 @@ app.use(cors());
 app.use(express.json({limit: "1mb"}))
 app.use(express.static('public'))
 
+// Routes
+// const aprovements = require("./aprovements");
+// app.use("/aprovements", aprovements);
+
 // Create databases (login and boss)
 const loginDB = new Datastore('login.db');
 const bossDB = new Datastore('boss.db');
 loginDB.loadDatabase();
 bossDB.loadDatabase();
+
+loginDB.insert.loginName = "Martin";
+loginDB.insert.loginPW = "2222";
 
 // insert formData to bossDB -> bossSpawned
 app.post('/bossAPI', (request, response) => {
@@ -45,5 +46,43 @@ app.post('/bossAPI', (request, response) => {
   })
 })
 
+app.get("/", (request, response) => {
+  console.log("success")
+  response.send("Test")
+})
 
+// insert LoginData to LoginDB -> success Login route
+app.post('/login', (request, response) => {
+  console.log('login success')
+  console.log(request.body)
+  console.log(response)
+  const loginNmAprove = request.body.loginName;
+  const loginPWAprove = request.body.loginPW
+  const loginObjInDB = loginDB.find(request.body.loginName)
+  console.log(loginObjInDB)
+  const isLogedIn = (loginNmAprove === loginObjInDB.loginName && loginPWAprove === loginObjInDB.loginPW) ? true : false;
 
+  return response.json({
+      isLogedIn,
+      _id: loginObjInDB._id,
+  });
+})
+
+// insert registerData to LoginDB -> successfull registrated register route
+app.post('/register', (request, response) => {
+  console.log('register success')
+  console.log(request.body)
+  console.log(response)
+  const registerData = {
+    loginName: request.body.registerName,
+    loginPW: request.body.registerPW,
+    userName: request.body.registerTwitch,
+    twitchToken: request.body.registerToken,
+  };
+  (!loginDB.includes(registerData)) ? loginDB.insert(loginData) : document.querySelector("p").textContent = "Username is registrated!" // read and insert
+  console.log(bossDB)
+  response.json({
+    loginName: request.body.loginName,
+    password: request.body.password,
+  })
+})
